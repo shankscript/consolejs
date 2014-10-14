b22f02f6616cc25863c5e6a156c4a312d2f1876d
Shashank Sharma <shashsh2@cisco.com>
2014-10-14 20:48:13 +0530

    updated

--- js/init.js
+++ js/init.js
@@ -1,53 +1,58 @@
-var textEl,
-    entries = [],
-    idx = 0;
-window.onload = function () {
-    textEl = document.querySelector("#console_text");
-    textEl.onkeydown = listener;
-};
+(function () {
+    var textEl,
+        entries = [],
+        idx = 0;
 
-function log(obj) {
-    var li = document.createElement("li");
-    li.innerHTML = ">>> " + obj.text;
-    li.classList.add(obj.type);
-    document.querySelector("#console_ul").appendChild(li);
-}
 
-function listener(e) {
-    //alert(e.keyCode);
-    switch (e.keyCode) {
-        case 13:
-            log(runJs(textEl.value));
-            entries.push(textEl.value);
-            textEl.value = "";
-            idx = 0;
-            break;
-        case 38:
-            console.log(entries[entries.length - idx - 1]);
-            if (entries[entries.length - idx - 1]) {
-                textEl.value = entries[entries.length - idx - 1];
-                idx++;
-            }
-            console.log(idx);
-            break;
-        case 40:
-            if (idx > 0) {
-                textEl.value = entries[entries.length - idx];
-                idx--;
-            }
-            break;
+    function log(obj) {
+        var li = document.createElement("li");
+        li.innerHTML = ">>> " + obj.text;
+        li.classList.add(obj.type);
+        document.querySelector("#console_ul").appendChild(li);
     }
-}
-function runJs(js) {
-    var ret = {};
-    try {
-        ret.text = eval(js);
-        ret.type = "default";
-    } catch (e) {
-        ret.text = e.toString();
-        ret.type = "Error";
-    }
-    return ret;
 
-}
+    function listener(e) {
+        //alert(e.keyCode);
+        switch (e.keyCode) {
+            case 13:
+                log(runJs(textEl.value));
+                entries.push(textEl.value);
+                textEl.value = "";
+                idx = 0;
+                break;
+            case 38:
+                console.log(entries[entries.length - idx - 1]);
+                if (entries[entries.length - idx - 1]) {
+                    textEl.value = entries[entries.length - idx - 1];
+                    idx++;
+                }
+                console.log(idx);
+                break;
+            case 40:
+                if (idx > 0) {
+                    textEl.value = entries[entries.length - idx];
+                    idx--;
+                }
+                break;
+        }
+    }
+
+    function runJs(js) {
+        var ret = {};
+        try {
+            ret.text = eval(js);
+            ret.type = "default";
+        } catch (e) {
+            ret.text = e.toString();
+            ret.type = "Error";
+        }
+        return ret;
+
+    }
+
+    window.onload = function () {
+        textEl = document.querySelector("#console_text");
+        textEl.onkeydown = listener;
+    };
+})();
 
